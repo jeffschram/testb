37c10138e9a838985406a6bcabcc0d7e68f5de4b
jeffschram <jeff@crushlovely.com>
2020-09-24 12:07:24 -0400

    update loader

--- testb-loader.js
+++ testb-loader.js
@@ -1,16 +1,20 @@
-(function() {
-
-  (function() {
-    var SkylineRiseBookmarklet, SkylineRisejQuery;
-    window.SkylineRiseBookmarkletVersion = "v2";
-    window.SkylineRiseBookmarkletDomain = "https://rise.skyline.is";
+(function () {
+  (function () {
+    var TestBBookmarklet, TestBjQuery;
+    window.TestBBookmarkletVersion = "";
+    window.TestBBookmarkletDomain = "https://jeffschram.github.io";
     // uncomment for local dev
-    // window.SkylineRiseBookmarkletDomain = "http://0.0.0.0:4567";
+    // window.TestBBookmarkletDomain = "http://0.0.0.0:4567";
 
-    SkylineRiseBookmarklet = document.createElement("script");
-    SkylineRiseBookmarklet.setAttribute("id", "SkylineRise-bookmarklet-script");
-    SkylineRiseBookmarklet.setAttribute("src", window.SkylineRiseBookmarkletDomain + "/javascripts/skyline-rise-bookmarklet-" + window.SkylineRiseBookmarkletVersion + ".js");
-    return document.body.appendChild(SkylineRiseBookmarklet);
+    TestBBookmarklet = document.createElement("script");
+    TestBBookmarklet.setAttribute("id", "TestB-bookmarklet-script");
+    TestBBookmarklet.setAttribute(
+      "src",
+      window.TestBBookmarkletDomain +
+        "/testb-bookmarklet" +
+        window.TestBBookmarkletVersion +
+        ".js"
+    );
+    return document.body.appendChild(TestBBookmarklet);
   })();
-
-}).call(this);
+}.call(this));
