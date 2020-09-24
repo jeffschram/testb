262aa74c614e3faa1ac3cb4f508900c9656687ec
jeffschram <jeff@crushlovely.com>
2020-09-24 13:29:16 -0400

    swap images

--- testb-bookmarklet.js
+++ testb-bookmarklet.js
@@ -5,6 +5,8 @@ var initSR = function () {
   alert("init!");
   pages = [];
   images = [];
+  imagesCounter = 0;
+  imagesLength = 0;
 
   if (localStorage.getItem("TB_Pages")) {
     pages = $.parseJSON(localStorage.getItem("TB_Pages"));
@@ -22,18 +24,37 @@ var initSR = function () {
   localStorage.setItem("TB_Pages", JSON.stringify(pages));
 
   $(document).on("keyup", function (event) {
-    alert("KEY:" + event.which);
     if (event.which === 191) {
-      alert("Go Time");
       addOverlay();
     }
+    if (event.which === 90) {
+      // back
+      swapImage(-1);
+    }
+    if (event.which === 88) {
+      // forward
+      swapImage(1);
+    }
   });
+};
+
+var swapImages = function (n) {
+  imagesCounter = imagesCounter + n;
+  if (imagesCounter < 0) {
+    imagesCounter = imagesLength - 1;
+  }
+  if (imagesCounter > imagesLength) {
+    imagesCounter = 0;
+  }
+  $("#TB_Overlay img").hide();
+  $("#TB_Overlay img").eq(imagesCounter).show();
 };
 
 var addOverlay = function () {
   var overlayHTML =
     "<div id='TB_Overlay' style='z-index:999999999999; position: fixed; top: 0; bottom: 0; left: 0; right: 0; background: black;'></div>";
   $("body").append(overlayHTML);
+  imagesLength = images.length;
   $.each(images, function (i, item) {
     $("#TB_Overlay").append(
       "<img src='" +
