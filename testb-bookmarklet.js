df8237736cd1239a4faee013c22161abbc104aac
jeffschram <jeff@crushlovely.com>
2020-09-24 12:36:06 -0400

    pages localstorage

--- testb-bookmarklet.js
+++ testb-bookmarklet.js
@@ -3,6 +3,21 @@
 
 var initSR = function () {
   alert("init!");
+  pages = [];
+
+  if (localStorage.getItem("TB_Pages")) {
+    pages = $.parseJSON(localStorage.getItem("TB_Pages"));
+    console.log("pages each");
+    $.each(pages, function (i, item) {
+      console.log("* pages each", pages[i]);
+    });
+  }
+
+  var pageURL = window.location.href;
+  pages.push(pageURL);
+  localStorage.setItem("TB_Pages", JSON.stringify(pages));
+
+  // JSON.stringify(overlays)
 };
 
 /* LOAD
